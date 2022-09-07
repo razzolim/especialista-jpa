f83343609f38265b9ba495f02760bd2a1f4b974f
Renan Azzolim <azzolimrenan2@gmail.com>
2022-09-07 12:39:20 -0300

    iniciando com JPA...

---
+++ src/main/java/com/razzolim/ecommerce/model/Cliente.java
@@ -0,0 +1,22 @@
+package com.razzolim.ecommerce.model;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Cliente {
+
+    @Id
+    @EqualsAndHashCode.Include
+    private Integer id;
+
+    private String nome;
+
+}
