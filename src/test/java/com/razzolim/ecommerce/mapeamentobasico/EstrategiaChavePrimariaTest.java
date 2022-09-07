1b6d5d89d4f21236b710881c7cb9535f608146ca
Renan Azzolim <azzolimrenan2@gmail.com>
2022-09-07 14:36:27 -0300

    @GeneratedValue(strategy = GenerationType.AUTO)

---
+++ src/test/java/com/razzolim/ecommerce/mapeamentobasico/EstrategiaChavePrimariaTest.java
@@ -0,0 +1,25 @@
+package com.razzolim.ecommerce.mapeamentobasico;
+
+import com.razzolim.ecommerce.EntityManagerTest;
+import com.razzolim.ecommerce.model.Categoria;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class EstrategiaChavePrimariaTest extends EntityManagerTest {
+
+    @Test
+    public void testarEstrategiaAuto() {
+        var categoria = new Categoria();
+        categoria.setNome("Eletrônicos");
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(categoria);
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        var categoriaVerificacao = entityManager.find(Categoria.class, categoria.getId());
+        Assert.assertNotNull(categoriaVerificacao);
+    }
+
+}
